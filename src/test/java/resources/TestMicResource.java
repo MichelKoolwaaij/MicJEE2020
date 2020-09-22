175850d196f6d7cba71815595e475e1852067c20
MichelKoolwaaij <michel.koolwaaij@han.nl>
2020-09-22 10:43:24 +0200

    added mocking

---
+++ src/test/java/resources/TestMicResource.java
@@ -0,0 +1,42 @@
+package resources;
+
+import dto.MicStudent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import services.StudentService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * Created by Michel Koolwaaij on 21-09-20.
+ */
+public class TestMicResource {
+
+    private MicResource sut;
+    private StudentService service;
+
+    @BeforeEach
+    public void init(){
+        sut = new MicResource();
+        service = mock(StudentService.class);
+
+    }
+
+    @Test
+    public void testGetJSONStudent(){
+        // arrange
+        var student = new MicStudent("Linda",10,"Linda@student.han.nl","555-1113");
+        sut.setService(service);
+        when(service.getStudent(10)).thenReturn(student);
+
+
+        // act
+        var returnStudent = sut.getJSONStudent(10);
+
+        // assert
+        Assertions.assertEquals(student, returnStudent);
+    }
+}
